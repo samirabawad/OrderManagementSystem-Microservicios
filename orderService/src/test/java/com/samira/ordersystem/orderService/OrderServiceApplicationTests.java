d48134d62918f0973d208b61c745633d4a8a7d9f
Samira Awad Labarca <136211595+samirabawad@users.noreply.github.com>
2024-11-06 03:21:05 -0300

    estructura base, banners y configuraciones de microservicios

---
+++ orderService/src/test/java/com/samira/ordersystem/orderService/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samira.ordersystem.orderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
